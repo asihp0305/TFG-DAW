1d38b7569a7f94975525fc6aa910c89373ce0f5a
Asier <asihp0305@gmail.com>
2026-01-20 12:53:29 +0100

    estructura navbar e imagenes

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *{
+            background-color: green
+        }
+    </style>
+</head>
+<body>
+    <!-- navbar con el logo,botones de las diferentes paginas ,y un boton de iniciar sesion que cambie segun este iniciada la sesion o no  -->
+    <div id="navBar">
+        <div id="DVlogo">
+            <img src="Imagenes/logoMinimalista.png" alt="logo Malpartida Dental" width="55px">
+        </div>
+        <div id="DVpaginas" >
+            <a href="#">CONÓCENOS</a><!-- link que lleva a la pagina de conocenos -->
+            <a href="#">PEDIR CITA</a><!-- link que lleva a la pagina de pedir cita(en esta controlar que esté la sesion iniciada para acceder o mandar a un login) -->
+        </div>
+        <div id="DVusuario">
+            <!-- hacer un if que en caso de que esté la sesion iniciada muestre el perfil del usuario o un boton para iniciar sesion -->
+            <button></button>
+        </div>
+    </div>
+</body>
+</html>
