ddb45ff53ae75583598570597b0255ff6d7b61a8
Asier <asihp0305@gmail.com>
2026-02-03 12:27:39 +0100

    Barra de navegacion mas menos done

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
+<?php session_start()?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/cssIndex.css">
     <title>Document</title>
-    <style>
-        *{
-            background-color: green
-        }
-    </style>
 </head>
 <body>
     <!-- navbar con el logo,botones de las diferentes paginas ,y un boton de iniciar sesion que cambie segun este iniciada la sesion o no  -->
     <div id="navBar">
         <div id="DVlogo">
-            <img src="Imagenes/logoMinimalista.png" alt="logo Malpartida Dental" width="55px">
+            <img src="Imagenes/logoMinimalista.png" alt="logo Malpartida Dental" width="65px" height="45px" >
         </div>
         <div id="DVpaginas" >
             <a href="#">CONÓCENOS</a><!-- link que lleva a la pagina de conocenos -->
@@ -22,8 +19,16 @@
         </div>
         <div id="DVusuario">
             <!-- hacer un if que en caso de que esté la sesion iniciada muestre el perfil del usuario o un boton para iniciar sesion -->
-            <button></button>
+             <?php 
+                if(isset($_SESSION["id"])){
+             ?>
+            <img src="Imagenes/iconoLog.png" alt="Sesion iniciada" width="55px" border-radius= "50%">
+             <?php }else{?>
+            <button id="IniSesion">Iniciar sesión</button>
+            <?php }?>
         </div>
     </div>
+
+    
 </body>
 </html>
