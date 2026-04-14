c49a641e5123bb1b95a9f9da86ed1b8250990514
Asier <asihp0305@gmail.com>
2026-04-14 20:43:07 +0200

    Creacion de galeria de imagenes para la bienvenida

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <!-- navbar con el logo,botones de las diferentes paginas ,y un boton de iniciar sesion que cambie segun este iniciada la sesion o no  -->
     <div id="navBar">
         <div id="DVlogo">
-            <img src="Imagenes/logoMinimalista.png" alt="logo Malpartida Dental" width="65px" height="45px" >
+            <img src="Imagenes/logo_minimalista.png" alt="logo Malpartida Dental" width="65px" height="45px" >
         </div>
         <div id="DVpaginas" >
             <a href="#">CONÓCENOS</a><!-- link que lleva a la pagina de conocenos -->
@@ -22,13 +22,23 @@
              <?php 
                 if(isset($_SESSION["id"])){
              ?>
-            <img src="Imagenes/iconoLog.png" alt="Sesion iniciada" width="55px" border-radius= "50%">
+            <img src="Imagenes/icono_log.png" alt="Sesion iniciada" width="55px" border-radius= "50%">
              <?php }else{?>
             <button id="IniSesion">Iniciar sesión</button>
             <?php }?>
         </div>
     </div>
-
-    
+    <div id="contenido">
+        <h1>BIENVENIDOS A MALPARTIDA DENTAL</h1>
+        <div id="imagenes_clinica">
+            <section>
+                <img src="Imagenes/recepcion.JPG" alt="imagen recepcion">
+                <img src="Imagenes/puerta_clinica.JPG" alt="imagen recepcion">
+                <img src="Imagenes/placa_clinica.JPG" alt="imagen recepcion">
+                <img src="Imagenes/sala_espera.JPG" alt="imagen recepcion">
+                <img src="Imagenes/consulta1.JPG" alt="imagen recepcion">
+            </section>
+        </div>
+    </div>
 </body>
 </html>
